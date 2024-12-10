a559edf4141e9a5a8f9fa1c5e958dc95dd4bc2ad
junya3 <obarajunya.0728@gmail.com>
2024-12-10 13:36:28 +0900

    indexをviewと変更

--- index.php
+++ index.php
@@ -1,32 +1,12 @@
 <?php
-require 'db.php';
+include_once dirname(__FILE__) . '/model/functions.php';
 
-$stmt = $pdo->query("SELECT * FROM recipes ORDER BY created_at DESC");
-$recipes = $stmt->fetchAll();
-?>
+// データの取得
+$sql = 'SELECT * FROM recipes';
+$recipes = getData($dbname, $user, $password, $sql);
 
-<!DOCTYPE html>
-<html lang="ja">
+// デフォルト値を設定
+$recipes = $recipes ?? [];
 
-<head>
-    <meta charset="UTF-8">
-    <title>レシピ一覧</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <h1>レシピ一覧</h1>
-    <p><a href="login.php">ログイン</a> | <a href="register.php">ユーザー登録</a></p>
-    <ul>
-        <?php foreach ($recipes as $recipe): ?>
-            <li>
-                <a href="recipe.php?id=<?= $recipe['id'] ?>"><?= htmlspecialchars($recipe['title']) ?></a>
-                <p><?= htmlspecialchars($recipe['ingredients']) ?></p>
-                <p><?= htmlspecialchars($recipe['instructions']) ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-    <p><a href="post_recipe.php">レシピを投稿</a></p>
-</body>
-
-</html>
+// Viewの読み込み
+include_once dirname(__FILE__) . '/view/index-view.php';
